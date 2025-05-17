2f0405059a773cd63f5fd4faf445b0bbb2b57b1c
Didi-121 <edidier11@outlook.com>
2025-05-17 04:23:28 -0600

    Test para la maniana

--- backend/NN/Embedings_generator.py
+++ backend/NN/Embedings_generator.py
@@ -2,32 +2,29 @@ import sqlite3
 import torch
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
-print("flag1")
+import os
+
 # Preparar modelo
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-# Imagen base
-img = Image.open("test2.png")
-inputs = processor(images=img, return_tensors="pt")
-embedding = model.get_image_features(**inputs)[0]  # [512] vector
 
 # Conexión con base de datos
 conn = sqlite3.connect("embeddings.db")
 c = conn.cursor()
 
-# Crear tabla
-c.execute('''
-CREATE TABLE IF NOT EXISTS embeddings (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    label TEXT,
-    vector BLOB
-)
-''')
+images = [f for f in os.listdir(r"backend/Data/chopped_products") if f.lower().endswith((".png"))]
 
-# Guardar embedding
-c.execute("INSERT INTO embeddings (label, vector) VALUES (?, ?)",
-          ("aceite nutri", embedding.detach().numpy().tobytes()))
-conn.commit()
-conn.close()
+# Imagen base
+for i in images:
+    img = Image.open(i)
+    inputs = processor(images=img, return_tensors="pt")
+    embedding = model.get_image_features(**inputs)[0]  # [512] vector
+    
+    # Guardar embedding
+    c.execute("INSERT INTO embeddings (label, vector) VALUES (?, ?)",
+            (os.path.splitext(os.path.basename(i))[0]
+    , embedding.detach().numpy().tobytes()))
+
+    conn.commit()
+    conn.close()
 
