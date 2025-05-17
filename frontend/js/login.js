b82894e2b18390f6e812de31a40f2447080e1790
Brianyan4717 <132787816+Brianyan4717@users.noreply.github.com>
2025-05-17 13:17:08 -0600

    Index completo y funcional

---
+++ frontend/js/login.js
@@ -0,0 +1,20 @@
+var admin_user = "Pedro Paramo";
+var admin_password = "12345678";
+var common_user = "Juan Rulfo";
+var common_password = "87654321";
+
+document.getElementById("login-button").addEventListener("click", () => login());
+
+
+function login() { 
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+
+    if (user === admin_user && password === admin_password) {
+        window.location.href = "admin.html";
+    } else if (user === common_user && password === common_password) {
+        window.location.href = "common.html";
+    } else {
+        alert("Invalid username or password");
+    }
+}
