b9ae373ff055dcd2694c06982f6de0eb5e60a020
SrPika <104371533+RichGN28@users.noreply.github.com>
2025-05-18 07:18:40 -0600

    test comunicacion

--- backend/Redes/routes.py
+++ backend/Redes/routes.py
@@ -8,22 +8,10 @@ CORS(app)
 @app.route("/detect", methods=["POST"])
 def add_detection():
     data = request.json
-    label = data.get("label")
-    confidence = data.get("confidence")
-    timestamp = data.get("timestamp")
+    label = data.get("nombre")
+    confidence = data.get("edad")
 
-
-    response = insert_detection(label, confidence, timestamp)
-
-    # 🛡️ Validación de que response existe
-    if response and response.data:
-        return jsonify({"message": "Detección guardada correctamente"}), 201
-
-    elif response and response.error:
-        return jsonify({"error": str(response.error)}), 400
-    else:
-        # Error grave: no hubo ni response ni error claro
-        return jsonify({"error": "Internal error or no response from Supabase"}), 500
+    return 201
 
 if __name__ == "__main__":
     app.run(debug=True)
