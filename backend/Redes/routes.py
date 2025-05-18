95401812f68658463086cd241acc7b8e298fada1
SrPika <104371533+RichGN28@users.noreply.github.com>
2025-05-18 06:51:45 -0600

    requierments

--- backend/Redes/routes.py
+++ backend/Redes/routes.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("detect", methods=["POST"])
+@app.route("/detect", methods=["POST"])
 def add_detection():
     data = request.json
     label = data.get("label")
