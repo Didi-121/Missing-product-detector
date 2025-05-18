745b2fedc2644d9e3cb3f45fdef60a0dd03acee8
Brianyan4717 <132787816+Brianyan4717@users.noreply.github.com>
2025-05-18 13:36:05 -0600

    hola mundo

--- backend/image_info.py
+++ backend/image_info.py
@@ -4,7 +4,7 @@ from NN.rotacion import estimate_rotation
 def image_info(pos):
     img = "temp.png"
     best_score, anaquel, charola, posicion, orientation = comparar_imagen(img)
-    #angulo = estimate_rotation(pos)
+    angulo = estimate_rotation(pos)
     actual_pos = anaquel + "," +  charola + "," + posicion
     orientations = {"1": "costado derecho", "2": "costado izquierdo", "3": "detrás", "4": "arriba", "5": "abajo"}
 
@@ -12,17 +12,7 @@ def image_info(pos):
         return {"ans": "El producto no es el que se espera",
                 "angle": angulo,
                 "pos": actual_pos }
-<<<<<<< HEAD
-    # elif (orientation != 0):
-    #     return {"ans": "El producto no está en la orientación esperada",
-    #             "pos": orientations[orientation]}
-    # elif (angulo > 45):
-    #     return {"ans": "El producto esta mal colocado", 
-    #             "angle": angulo}
-=======
->>>>>>> origin/redes
     else:
-        
         return {"ans": "El producto está bien colocado", "angle": angulo,"pos": actual_pos }
     
     
